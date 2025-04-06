ef6d64a496db1d3b7f7cdb1d070d81e6eeaf29ff
edsay12 <edvandearaujo2@hotmail.com>
2025-04-06 16:13:25 -0300

    feat: section mitos e verdades

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import CanaisDeAjuda from "./components/canais-de-ajuda";
 import HomeBanner from "./components/home-banner";
+import SectionMitosEVerdades from "./components/mitos-e-verdades";
 import SectionRealidadeEmNumeros from "./components/realidade-em-numeros";
 import SectionTiposDeViolencia from "./components/reconheca-os-tipo-de-violencia";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <SectionRealidadeEmNumeros />
       <SectionTiposDeViolencia />
       <CanaisDeAjuda />
+      <SectionMitosEVerdades />
     </>
   );
 }
