035e689bf2887e21567f3febb55772c1fdd07f61
edsay12 <edvandearaujo2@hotmail.com>
2025-04-08 00:43:19 -0300

    feat: adicionando animação nas formas de violencia

--- app/tipos-de-violencia/components/formas-de-violencia.tsx
+++ app/tipos-de-violencia/components/formas-de-violencia.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Section from "@/components/Section";
 
 import ViolenciaCard from "@/components/violencia-card";
+import { fadeUp } from "@/lib/animation/animations";
 import { formasDeViolencia } from "@/lib/constants/formasDeViolencia";
+import { motion } from "framer-motion";
 
 function FormasDeViolencia() {
   return (
@@ -11,7 +14,15 @@ function FormasDeViolencia() {
     >
       <div className="flex flex-col gap-5 mt-12">
         {formasDeViolencia.map((forma, i) => (
-          <ViolenciaCard key={i} {...forma} />
+          <motion.div
+            key={i}
+            variants={fadeUp}
+            initial="hidden"
+            whileInView={"visible"}
+            viewport={{ once: true }}
+          >
+            <ViolenciaCard {...forma} />
+          </motion.div>
         ))}
       </div>
     </Section>
--- app/tipos-de-violencia/components/informacao-inicial.tsx
+++ app/tipos-de-violencia/components/informacao-inicial.tsx
@@ -1,27 +1,41 @@
+"use client";
+
 import Aviso from "@/components/aviso";
 import Section from "@/components/Section";
 import { TriangleAlert } from "lucide-react";
+import { motion } from "framer-motion";
+import { fadeUp } from "@/lib/animation/animations";
 
 function InformacaoInicial() {
   return (
-    <Section className="max-w-[900px] mx-auto ">
-      <p className=" text-muted-foreground text-sm mb-5">
-        A Lei Maria da Penha (Lei nº 11.340/2006) define cinco formas de
-        violência doméstica e familiar contra a mulher: física, psicológica,
-        sexual, patrimonial e moral. Além dessas, existem outras formas de
-        violência que afetam as mulheres em diferentes contextos sociais.
-      </p>
-      <p className="text-muted-foreground text-sm mb-5">
-        Reconhecer esses tipos de violência é fundamental para que as mulheres
-        possam identificar situações abusivas e buscar ajuda. Também é
-        importante para que a sociedade como um todo possa combater essas
-        práticas.
-      </p>
-      <Aviso
-        title="Importante"
-        icon={<TriangleAlert className="h-5 w-5 text-purple-800" />}
-        description="Se você está em situação de perigo imediato, ligue para o 190 (Polícia Militar). Para orientações e denúncias, ligue para o 180 (Central de Atendimento à Mulher), um serviço gratuito e confidencial disponível 24 horas por dia."
-      />
+    <Section className="max-w-[900px] mx-auto">
+      {[...Array(2)].map((_, i) => (
+        <motion.p
+          key={i}
+          className="text-muted-foreground text-sm mb-5"
+          variants={fadeUp}
+          initial="hidden"
+          animate="visible"
+          custom={i}
+        >
+          {i === 0
+            ? "A Lei Maria da Penha (Lei nº 11.340/2006) define cinco formas de violência doméstica e familiar contra a mulher: física, psicológica, sexual, patrimonial e moral. Além dessas, existem outras formas de violência que afetam as mulheres em diferentes contextos sociais."
+            : "Reconhecer esses tipos de violência é fundamental para que as mulheres possam identificar situações abusivas e buscar ajuda. Também é importante para que a sociedade como um todo possa combater essas práticas."}
+        </motion.p>
+      ))}
+
+      <motion.div
+        variants={fadeUp}
+        initial="hidden"
+        animate="visible"
+        custom={2}
+      >
+        <Aviso
+          title="Importante"
+          icon={<TriangleAlert className="h-5 w-5 text-purple-800" />}
+          description="Se você está em situação de perigo imediato, ligue para o 190 (Polícia Militar). Para orientações e denúncias, ligue para o 180 (Central de Atendimento à Mulher), um serviço gratuito e confidencial disponível 24 horas por dia."
+        />
+      </motion.div>
     </Section>
   );
 }
