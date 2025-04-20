affbd36d0edb40b44217b517cd932a7dffb96cde
edsay12 <edvandearaujo2@hotmail.com>
2025-04-20 17:33:01 -0300

    feat: adicionando o melhor modelo e atualizando as colunas

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ colunas_modelo = joblib.load("colunas_modelo.pkl")
 class EntradaModelo(BaseModel):
     ANO: int
     NU_IDADE_N: int
-    CS_RACA: str
-    UF: str
-    LOCAL_OCOR: str
-    GRUPO_IDADE: str
-    TIPO_RELACAO: str
     AUTOR_SEXO: int
     OUT_VEZES: int
+    UF: str
+    CS_RACA: str
+    CS_ESCOL_N: str
+    LOCAL_OCOR: str
+    TIPO_RELACAO: str
 
 @app.post("/predict")
 def predict(dados: EntradaModelo):
@@ -32,8 +32,9 @@ def predict(dados: EntradaModelo):
     input_dict[f"CS_RACA_{input_dict.pop('CS_RACA')}"] = 1
     input_dict[f"UF_{input_dict.pop('UF')}"] = 1
     input_dict[f"LOCAL_OCOR_{input_dict.pop('LOCAL_OCOR')}"] = 1
-    input_dict[f"GRUPO_IDADE_{input_dict.pop('GRUPO_IDADE')}"] = 1
     input_dict[f"TIPO_RELACAO_{input_dict.pop('TIPO_RELACAO')}"] = 1
+    input_dict[f"AUTOR_SEXO_{input_dict.pop('AUTOR_SEXO')}"] = 1
+    input_dict[f"CS_ESCOL_N_{input_dict.pop('CS_ESCOL_N')}"] = 1
 
     # Transforma em DataFrame
     df = pd.DataFrame([input_dict])
@@ -57,7 +58,7 @@ def predict(dados: EntradaModelo):
         2: "outros",
         3: "psicológica",
     }
-    
+
     resultado = {
         classes[i]: round(float(prob * 100),2)
         for i, prob in enumerate(predicao)
