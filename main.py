8bd3bb59f06244b9050b7ff02d785a481202dc3e
matheusleobelo <bipoladogamer@gmail.com>
2025-04-21 15:07:19 -0300

    back conectado com o front

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import pandas as pd
-import joblib
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import joblib
+import pandas as pd
 from pydantic import BaseModel
 
 app = FastAPI()
 
-# Carrega o modelo treinado
-modelo = joblib.load("model_lgb.sav")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Carrega a lista de colunas que o modelo espera
+modelo = joblib.load("model_lgb.sav")
 colunas_modelo = joblib.load("colunas_modelo.pkl")
 
-# Define o schema de entrada da API
 class EntradaModelo(BaseModel):
     ANO: int
     NU_IDADE_N: int
@@ -25,10 +30,7 @@ class EntradaModelo(BaseModel):
 
 @app.post("/predict")
 def predict(dados: EntradaModelo):
-    # Converte entrada JSON em dicionário
-    input_dict = dados.model_dump()
-
-    # Renomeia os campos categóricos para ficarem no formato esperado do one-hot
+    input_dict = dados.dict()
     input_dict[f"CS_RACA_{input_dict.pop('CS_RACA')}"] = 1
     input_dict[f"UF_{input_dict.pop('UF')}"] = 1
     input_dict[f"LOCAL_OCOR_{input_dict.pop('LOCAL_OCOR')}"] = 1
@@ -36,22 +38,16 @@ def predict(dados: EntradaModelo):
     input_dict[f"AUTOR_SEXO_{input_dict.pop('AUTOR_SEXO')}"] = 1
     input_dict[f"CS_ESCOL_N_{input_dict.pop('CS_ESCOL_N')}"] = 1
 
-    # Transforma em DataFrame
     df = pd.DataFrame([input_dict])
 
-    # Garante que todas as colunas esperadas estão presentes
     for col in colunas_modelo:
         if col not in df.columns:
             df[col] = 0
 
-    # Reordena as colunas na ordem do treino
     df = df[colunas_modelo]
 
-    # Faz a predição
     predicao = modelo.predict_proba(df)[0]
-    print("console",predicao)
 
-    #Mapeamento das classes
     classes = {
         0: "fisica",
         1: "nenhuma",
@@ -60,10 +56,8 @@ def predict(dados: EntradaModelo):
     }
 
     resultado = {
-        classes[i]: round(float(prob * 100),2)
+        classes[i]: round(float(prob * 100), 2)
         for i, prob in enumerate(predicao)
     }
-
-    
 
     return resultado
